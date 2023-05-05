01129c95c77cf313f370be0248ed8cbf9c24967f
Prince29chouhan <princechouhan98765@gmail.com>
2023-05-05 18:26:31 +0530

    project commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactComponent} from './contact/contact.component';
+import { DashboardComponent} from './dashboard/dashboard.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    component: DashboardComponent
+  },
+  {
+    path:'contact',
+    component: ContactComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
